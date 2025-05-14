b52eb90250dc09e84d2ff1f2d641089a90b8dfcb
lailamacho <lailamachova@gmail.com>
2025-05-14 22:12:20 +0200

    dash checkpoint-1

--- streamlit/pages/meteorites.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-@st.cache_data
-def from_data_file(filename):
-    """Load data from a CSV file and return it as a DataFrame."""
-    return pd.read_csv(filename)
-
